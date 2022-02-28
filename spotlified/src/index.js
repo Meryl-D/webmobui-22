5f007bdf27ee31cc4330ee037bbc464b29531d57
Meryl Dubois <meryl.dubois@heig-vd.ch>
2022-02-28 11:36:08 +0100

    add header, footer and home

--- spotlified/src/index.js
+++ spotlified/src/index.js
@@ -1,1 +1,9 @@
 import './css/index.css'
+
+const searchSubmit = document.querySelector('.search-submit');
+console.log(searchSubmit);
+const searchBox = document.querySelector('.search-box');
+
+searchSubmit.addEventListener('click', evt => {
+    searchBox.classList.toggle('toggle-search');
+});
